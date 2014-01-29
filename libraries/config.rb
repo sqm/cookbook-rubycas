3b253e100f2e153fdcdf940e749a74772d17d000
Paul Welch <paul@pwelch.net>
2014-01-29 16:50:31 -0500

    Remove unused respond_to_missing method
    The `#respond_to_missing?` method because there is nothing the
    class will not respond to.
    
    [24805]

--- libraries/config.rb
+++ libraries/config.rb
@@ -11,10 +11,6 @@ module Rubycas
       def method_missing(method, *args, &block)
         config[method.to_s]
       end
-
-      def respond_to_missing?(method, include_private = false)
-        config.has_key?(method.to_s) || super
-      end
     end
   end
 end
